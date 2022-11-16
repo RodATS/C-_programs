cea451e6585d4b17aca5067de3d7e88d23533b68
RodATS <77297145+RodATS@users.noreply.github.com>
2022-11-15 22:31:02 -0500

    Update dijsktra.cpp

--- Curso_Competitva/Dijsktra/dijsktra.cpp
+++ Curso_Competitva/Dijsktra/dijsktra.cpp
@@ -2,27 +2,19 @@
 using namespace std;
 #include <limits.h>
 
-// Number of vertices in the graph
 #define V 9
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+int minDistance(int dist[], bool Visitados[])
 {
-
-	// Initialize min value
-	int min = INT_MAX, min_index;
+	int min = INT_MAX, index;
 
 	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
+		if (Visitados[v] == false && dist[v] <= min)
+			min = dist[v], index = v;
 
-	return min_index;
+	return index;
 }
 
-// A utility function to print the constructed distance
-// array
 void printSolution(int dist[], int s)
 {
 	cout << "Origen \t Destino \t Distancia" << endl;
@@ -32,30 +24,21 @@ void printSolution(int dist[], int s)
 
 void dijkstra(int graph[V][V], int src)
 {
-	int dist[V]; // The output array. dist[i] will hold the
-				// shortest
-	// distance from src to i
+	int dist[V]; 
+	bool Visitados[V];
 
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is
-					// included in shortest
-	// path tree or shortest distance from src to i is
-	// finalized
-
-	// Initialize all distances as INFINITE and stpSet[] as
-	// false
 	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
+		dist[i] = INT_MAX, Visitados[i] = false;
 
-	// Distance of source vertex from itself is always 0
+	//Ir a si mismo siemore es 0
 	dist[src] = 0;
 
-	// Find shortest path for all vertices
 	for (int count = 0; count < V - 1; count++) {
-		int u = minDistance(dist, sptSet);
+		int u = minDistance(dist, Visitados);
 
-		sptSet[u] = true;
+		Visitados[u] = true;
 		for (int v = 0; v < V; v++)
-			if (!sptSet[v] && graph[u][v]
+			if (!Visitados[v] && graph[u][v]
 				&& dist[u] != INT_MAX
 				&& dist[u] + graph[u][v] < dist[v])
 				dist[v] = dist[u] + graph[u][v];
